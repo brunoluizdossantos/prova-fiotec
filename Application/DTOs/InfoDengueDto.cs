542e66f491c1f99858d50fa3299716eee2390a1b
Bruno Luiz <bluiz@wearskypro.com>
2025-05-17 17:29:38 -0300

    Ajustado exibição dos dados epidemológicos

--- Application/DTOs/InfoDengueDto.cs
+++ Application/DTOs/InfoDengueDto.cs
@@ -1,41 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.DTOs;
+﻿namespace Application.DTOs;
 
 public class InfoDengueDto
 {
-	//"data_iniSE": 1512864000000,
-	//"SE": 201750,
-	//"casos_est": 67.0,
-	//"casos_est_min": 67,
-	//"casos_est_max": 67,
-	public int Casos { get; set; }
-	//"p_rt1": 0.6232580543,
-	//"p_inc100k": 0.992914021,
-	//"Localidade_id": 0,
-	//public int Nivel { get; set; }
-	//public int Id { get; set; }
-	//"versao_modelo": "2023-08-20",
-	//"tweet": 6.0,
-	//"Rt": 1.056363821,
-	//"pop": 6747815.0,
-	//"tempmin": 22.7142857143,
-	//"umidmax": 84.1175907143,
-	//"receptivo": 1,
-	//"transmissao": 0,
-	//"nivel_inc": 0,
-	//"umidmed": 77.2555742857,
-	//"umidmin": 69.5498814286,
-	//"tempmed": 23.6238094286,
-	//"tempmax": 24.8571428571,
-	//"casprov": 53,
-	//"casprov_est": null,
-	//"casprov_est_min": null,
-	//"casprov_est_max": null,
-	//"casconf": null,
-	//"notif_accum_year": 4392
+	public long? data_iniSE { get; set; }
+	public long? SE { get; set; }
+	public decimal? casos_est { get; set; }
+	public decimal? casos_est_min { get; set; }
+	public decimal? casos_est_max { get; set; }
+	public long? casos { get; set; }
+	public decimal? p_rt1 { get; set; }
+	public decimal? p_inc100k { get; set; }
+	public long? Localidade_id { get; set; }
+	public int? nivel { get; set; }
+	public long? id { get; set; }
+	public string versao_modelo { get; set; } = String.Empty;
+	public decimal? tweet { get; set; }
+	public decimal? Rt { get; set; }
+	public decimal? pop { get; set; }
+	public decimal? tempmin { get; set; }
+	public decimal? umidmax { get; set; }
+	public long? receptivo { get; set; }
+	public long transmissao { get; set; }
+	public long? nivel_inc { get; set; }
+	public decimal? umidmed { get; set; }
+	public decimal umidmin { get; set; }
+	public decimal? tempmed { get; set; }
+	public decimal? tempmax { get; set; }
+	public long? casprov { get; set; }
+	public long? casprov_est { get; set; }
+	public long? casprov_est_min { get; set; }
+	public long? casprov_est_max { get; set; }
+	public long? casconf { get; set; }
+	public long	notif_accum_year { get; set; }
 }
